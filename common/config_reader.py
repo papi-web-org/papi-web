a81467309d8e9b95496b028e556a1ba39522f90d
Sammy Plat <sammy@sammyplat.fr>
2023-09-06 01:44:14 +0200

    Removed ability for empty lines in values
    Taken from https://docs.python.org/3/library/configparser.html#customizing-parser-behaviour
    The issue can arise with the following config:
    
    [Section]
    key = multiline
      value with a gotcha
    
     this = is still a part of the multiline value of 'key'
    
    Sine "this" is not on the left, the whole line is parsed as part of the value of "key"

--- common/config_reader.py
+++ common/config_reader.py
@@ -15,7 +15,7 @@ TMP_DIR: Path = Path('tmp')
 # https://docs.python.org/3/library/configparser.html
 class ConfigReader(ConfigParser):
     def __init__(self, ini_file: Path, silent: bool):
-        super().__init__(interpolation=None)
+        super().__init__(interpolation=None, empty_lines_in_values=False)
         self.__ini_file: Path = ini_file
         ini_marker_dir: Path = Path(TMP_DIR, self.ini_file.parents[0])
         ini_marker_file: Path = Path(ini_marker_dir, self.ini_file.name + '.read')
