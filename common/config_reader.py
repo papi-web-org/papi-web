269fb3a8bbcdb29927dd86fc44d412ceb9557cb1
Sammy Plat <sammy@sammyplat.fr>
2023-09-15 03:46:32 +0200

    Added a check to detect failing to read a file
    This could happen for any reason from the file not existing, a symlink switching hammering,
    permission errors, which prevents a file from being read correctly.
    
    Also used the parent directory directly (instead of accessing the first of the parents),
    and used an f-string instead of concatenation

--- common/config_reader.py
+++ common/config_reader.py
@@ -17,8 +17,8 @@ class ConfigReader(ConfigParser):
     def __init__(self, ini_file: Path, silent: bool):
         super().__init__(interpolation=None, empty_lines_in_values=False)
         self.__ini_file: Path = ini_file
-        ini_marker_dir: Path = Path(TMP_DIR, self.ini_file.parents[0])
-        ini_marker_file: Path = Path(ini_marker_dir, self.ini_file.name + '.read')
+        ini_marker_dir: Path = Path(TMP_DIR, self.ini_file.parent)
+        ini_marker_file: Path = Path(ini_marker_dir, f'{self.ini_file.name}.read')
         self.__infos: List[str] = []
         self.__warnings: List[str] = []
         self.__errors: List[str] = []
@@ -37,7 +37,16 @@ class ConfigReader(ConfigParser):
             else:
                 logger.info(f'Configuration file [{self.ini_file}] has been modified, reloading...')
         try:
-            self.read(self.__ini_file, encoding='utf8')
+            files_read = self.read(self.__ini_file, encoding='utf8')
+            # NOTE(Amaras) There could still be a problem leading to not
+            # getting a configuration.
+            # Since a file raising an OSError is ignored, a file not existing
+            # (due to a TOC/TOU bug), a file being changed to a directory
+            # (e.g. by symlink switching), or a permission error (not accounted
+            # for by previous checks)
+            if str(self.__ini_file) not in files_read:
+                self._add_error(f'Could not read: {self.__ini_file}')
+                return
             if not ini_marker_dir.is_dir():
                 ini_marker_dir.mkdir(parents=True)
             ini_marker_file.touch()
