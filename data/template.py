9ad31435f736d96420254e7ef0709b52298d4aa9
Sammy Plat <sammy@sammyplat.fr>
2023-09-25 18:14:35 +0200

    Forgot to change template class

--- data/template.py
+++ data/template.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict
 from logging import Logger
 
 from common.logger import get_logger
@@ -9,7 +9,7 @@ logger: Logger = get_logger()
 class Template:
     def __init__(self, id: str):
         self.__id = id
-        self.__data: Dict[Optional[str], Dict[str, str]] = {}
+        self.__data: Dict[str | None, Dict[str, str]] = {}
 
     @property
     def id(self) -> str:
@@ -19,7 +19,7 @@ class Template:
     def data(self) -> Dict[str, Dict[str, str]]:
         return self.__data
 
-    def add_data(self, section: Optional[str], key: str, value: str):
+    def add_data(self, section: str | None, key: str, value: str):
         if section not in self.__data:
             self.__data[section] = {key: value, }
         else:
