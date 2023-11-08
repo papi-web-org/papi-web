05faf707d4196c0717def0990663ae5ca2505cf4
pascalaubry <pascalaubry35@gmail.com>
2023-11-08 22:07:27 +0100

    Added class RotatorBuilder to lighten class Event

--- data/event.py
+++ data/event.py
@@ -1,4 +1,3 @@
-import datetime
 import re
 import time
 from configparser import SectionProxy
@@ -12,7 +11,7 @@ from common.config_reader import ConfigReader
 from common.logger import get_logger
 from data.board import Board
 from data.result import Result
-from data.rotator import Rotator, ROTATOR_DEFAULT_DELAY
+from data.rotator import Rotator, RotatorBuilder
 from data.screen import ScreenSet, ScreenBoards, ScreenPlayers, ScreenResults, AScreen
 from data.screen import ScreenType
 from data.template import Template
@@ -1247,84 +1246,12 @@ class Event:
         return screen_sets
 
     def _build_rotators(self):
-        rotator_ids: list[str] = self.reader.get_subsection_keys_with_prefix('rotator')
-        if not rotator_ids:
-            self.reader.add_debug('aucun écran rotatif déclaré', 'rotator.*')
-            return
-        for rotator_id in rotator_ids:
-            self._build_rotator(rotator_id)
-        if not len(self.rotators):
+        rotator_builder: RotatorBuilder = RotatorBuilder(self.reader, self.screens, self.screens_by_family_id)
+        for rotator_id in rotator_builder.read_rotator_ids():
+            if rotator := rotator_builder.build_rotator(rotator_id):
+                self.rotators[rotator_id] = rotator
+        if not self.rotators:
             self.reader.add_debug('aucun écran rotatif défini')
-
-    def _build_rotator(self, rotator_id: str):
-        section_key = f'rotator.{rotator_id}'
-        rotator_section = self.reader[section_key]
-        section_keys: list[str] = ['screens', 'families', 'delay', ]
-        for key in rotator_section:
-            if key not in section_keys:
-                self.reader.add_warning('option inconnue', section_key, key)
-        key = 'delay'
-        default_delay: int | None = ROTATOR_DEFAULT_DELAY
-        delay: int = default_delay
-        if key not in rotator_section:
-            self.reader.add_debug(
-                f'option absente, par défaut [{default_delay}]',
-                section_key,
-                key
-            )
-        else:
-            delay = self.reader.getint_safe(section_key, key, minimum=1)
-            if delay is None:
-                self.reader.add_warning(
-                    f'un entier positif non nul est attendu, par défaut '
-                    f'[{default_delay}]',
-                    section_key,
-                    key
-                )
-        if 'screens' not in rotator_section and 'families' not in rotator_section:
-            self.reader.add_info(
-                'au moins une option parmi [screens] et [families] doit être '
-                'définie, écran rotatif ignoré',
-                section_key
-            )
-            return
-        screens: list[AScreen] = []
-        key = 'families'
-        if key in rotator_section:
-            for family_id in str(rotator_section.get(key)).replace(' ', '').split(','):
-                if family_id:
-                    if family_id not in self.screens_by_family_id:
-                        self.reader.add_warning(
-                            f"la famille [{family_id}] n'existe pas, ignorée",
-                            section_key,
-                            key
-                        )
-                    else:
-                        screens += self.screens_by_family_id[family_id]
-        key = 'screens'
-        if key in rotator_section:
-            for screen_id in str(rotator_section.get(key)).replace(' ', '').split(','):
-                if screen_id:
-                    if screen_id not in self.screens:
-                        self.reader.add_warning(
-                            f"l'écran [{screen_id}] n'existe pas, ignoré",
-                            section_key,
-                            key
-                        )
-                    elif self.screens[screen_id] not in screens:
-                        screens.append(self.screens[screen_id])
-
-        if 'screens' not in rotator_section and 'families' not in rotator_section:
-            self.reader.add_warning(
-                'au moins une des deux options [screens] ou [families] doit '
-                'être utilisée, écran rotatif ignoré',
-                section_key
-            )
-            return
-        if not screens:
-            self.reader.add_warning('aucun écran, écran rotatif ignoré', section_key, key)
-            return
-        self.rotators[rotator_id] = Rotator(rotator_id, delay, screens)
 
     def store_result(self, tournament: Tournament, board: Board, result: int):
         results_dir: Path = Result.results_dir(self.id)
