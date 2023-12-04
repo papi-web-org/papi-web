5a92da05b69e81ac82cc252a55b643bd35ee49a3
pascalaubry <pascalaubry35@gmail.com>
2023-12-04 23:51:37 +0100

    Finalized 2.1-rc11

--- common/papi_web_config.py
+++ common/papi_web_config.py
@@ -13,7 +13,7 @@ from common.logger import get_logger, configure_logger
 
 logger: Logger = get_logger()
 
-PAPI_WEB_VERSION: str = '2.1-rc10'
+PAPI_WEB_VERSION: str = '2.1-rc11'
 
 PAPI_WEB_URL = 'https://github.com/pascalaubry/papi-web'
 
