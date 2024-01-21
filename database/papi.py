7c1bb36e15a13cd7070d760866c07554cf849965
Sammy Plat <sammy@sammyplat.fr>
2024-01-21 01:26:13 +0100

    Update for performance and added explanation

--- database/papi.py
+++ database/papi.py
@@ -173,7 +173,8 @@ class PapiDatabase(AccessDatabase):
         }
         for round in range(1, 25):
             data[f'Rd{round:0>2}Adv'] = None
-            if round not in player.skipped_rounds and not player.check_in:
+            # If a player is not checked-in, treat them as forfeiting all rounds
+            if not player.check_in and round not in player.skipped_rounds:
                 data[f'Rd{round:0>2}Res'] = Result.NOT_PAIRED.to_papi_value
                 data[f'Rd{round:0>2}Cl'] = 'R'
             elif player.check_in or player.skipped_rounds[round] == 0.0:
